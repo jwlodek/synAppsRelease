fd35053846aa0988be4081c8bcf9268d94b1b85c
Kaz Gofron <kgofron@comcast.net>
2018-02-16 19:34:26 -0500

    Repair ADSupport of areaDetector 3.2

---
+++ SCRIPTS/ADSupportRepair/DexelaCompile/Malfomred_cmstypes.c
@@ -0,0 +1,11 @@
+Sqr-1 IXS@IXS_MCM /cygdrive/c/epics/synApps/support/areaDetector-3-2/ADSupport/supportApp/GraphicsMagickSrc/lcms/src
+$ grep -n -axv '.*' cmstypes.c
+965:    //(see clause 4.1 for the definition of ¦aligned¦). Because the Unicode language
+3063://The device representation corresponds to the header¦s ¦color space of data¦ field.
+3064://This representation should be consistent with the ¦number of device components¦
+3066://The PCS representation corresponds to the header¦s PCS field. The PCS representation
+3811://CIE ¦absolute¦ illuminant white point tristimulus values and CIE ¦absolute¦
+3898:// The first curve segment always starts at ¦Infinity, and the last curve segment always ends at +Infinity. The
+4174:// array = [e11, e12, ¦, e1P, e21, e22, ¦, e2P, ¦, eQ1, eQ2, ¦, eQP, e1, e2, ¦, eQ]
+4673:            // Y = (Max ¦ Min) * (X ^ Gamma) + Min
+4676:            // a = (Max ¦ Min) ^ ( 1 / Gamma)
